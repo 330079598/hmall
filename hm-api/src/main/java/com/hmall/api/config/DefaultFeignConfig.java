e4e28deebcfe1c51a8ac7b565e6bf3aae17ce569
stone <330079598@qq.com>
2025-01-14 13:03:59 +0800

    open-feign 微服务之间处理请求头问题

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,12 +1,34 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
 
+    /**
+     * open-feign 日志
+     */
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    /**
+     * open-feign 拦截器：处理微服务之间请求头携带用户信息问题
+     */
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                Long user = UserContext.getUser();
+                if (user != null) {
+                    template.header("user-info", user.toString());
+                }
+            }
+        };
     }
 }
