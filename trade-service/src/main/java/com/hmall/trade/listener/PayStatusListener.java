e15868273afa66d32ed4936953984dcfed6d161d
stone <330079598@qq.com>
2025-02-11 16:06:28 +0800

    延迟队列

--- trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
+++ trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
@@ -1,5 +1,6 @@
 package com.hmall.trade.listener;
 
+import com.hmall.trade.domain.po.Order;
 import com.hmall.trade.service.IOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.Exchange;
@@ -23,6 +24,14 @@ public class PayStatusListener {
             key = "pay.success"
     ))
     public void listenPaySuccess(Long orderId) {
+        // 查询订单
+        Order order = orderService.getById(orderId);
+        // 判断订单状态，是否为未支付
+        if (order == null || order.getStatus() != 1) {
+            // 不做处理
+            return;
+        }
+        // 修改订单为已支付
         orderService.markOrderPaySuccess(orderId);
     }
 }
